f4883e5ae4a5ec15434a347d4455f33f73d331c7
Avery Little <averywlittle@gmail.com>
2020-03-31 22:15:17 -0400

    Created inventory screen and heal function for potion

--- input_handlers.py
+++ input_handlers.py
@@ -1,6 +1,17 @@
 import tcod as libtcod
+from game_states import GameStates
 
-def handle_keys(key):
+def handle_keys(key, game_state):
+    if game_state == GameStates.PLAYERS_TURN:
+        return handle_player_turn_keys(key)
+    elif game_state == GameStates.PLAYER_DEAD:
+        return handle_player_dead_keys(key)
+    elif game_state == GameStates.SHOW_INVENTORY:
+        return handle_inventory_keys(key)
+    
+    return {}
+
+def handle_player_turn_keys(key):
     key_char = chr(key.c)
 
     # Movement handle_keys
@@ -23,6 +34,9 @@ def handle_keys(key):
 
     if key_char == 'g':
         return {'pickup': True}
+    
+    elif key_char == 'i':
+        return {'show_inventory': True}
 
     if key.vk == libtcod.KEY_ENTER and key.lalt:
         # Alt+Enter: toggle full screen
@@ -33,4 +47,34 @@ def handle_keys(key):
         return {'exit': True}
 
     # No key was pressed
+    return {}
+
+def handle_player_dead_keys(key):
+    key_char = chr(key.c)
+
+    if key_char == 'i':
+        return {'show_inventory': True}
+
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        # Alt+Enter: toggle fullscreen
+        return {'fullscreen': True}
+    elif key.vk == libtcod.KEY_ESCAPE:
+        # Exit the menu
+        return {'exit': True}
+    
+    return {}
+
+def handle_inventory_keys(key):
+    index = key.c - ord('a')
+
+    if index >= 0:
+        return {'inventory_index': index}
+    
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        # Alt+Enter: toggle fullscreen
+        return {'fullscreen': True}
+    elif key.vk == libtcod.KEY_ESCAPE:
+        # Exit the menu
+        return {'exit': True}
+
     return {}
